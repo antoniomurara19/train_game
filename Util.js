123b110b52ee598d53a1eb3c8c25d7bca1d5428f
antonio murara zabala <antoniomurar4@gmail.com>
2023-08-02 12:23:47 -0300

    adicionando imagem, audio e texto

--- Util.js
+++ Util.js
@@ -60,12 +60,12 @@ class Object{
         des.fillStyle = this.atrib
         des.strokeStyle = 'black'
         des.moveTo(this.x+15,this.y-2)
-        des.lineTo(this.x+15,this.y-15)
+        des.lineTo(this.x+15,this.y-5)
+        des.lineTo(this.x-15,this.y-5)
         des.lineTo(this.x-15,this.y-15)
-        des.lineTo(this.x-15,this.y-25)
-        des.lineTo(this.x+65,this.y-25)
         des.lineTo(this.x+65,this.y-15)
-        des.lineTo(this.x+35,this.y-15)
+        des.lineTo(this.x+65,this.y-5)
+        des.lineTo(this.x+35,this.y-5)
         des.lineTo(this.x+35,this.y-2)
         des.closePath()
         des.stroke()
@@ -134,6 +134,8 @@ class Object{
         des.stroke()
         des.fill()
     }
+
+    
 }
 
 // classe carro
@@ -141,32 +143,364 @@ class Object{
 class Train extends Object{
     dir = 0
     move(){
+
         this.x += this.dir
+
+        if(this.x <= 16){
+            thix.x = 16
+        }else if(this.x >= 550){
+            thix.x = 550
+        }
+    }
+
+    des_obj(){
+        let img = new Image()
+        img.src = this.atrib
+        des.drawImage(img,this.x,this.y,this.w,this.h)
     }
 }
 
 class Carro1 extends Object{
     move(){
-        this.y += 1
+        this.y += 3
         if(this.y >= 610){
             this.y = -100
             this.x = (Math.random() * (545 - 2 + 1) + 2)
         }
+    }
+    des_obj(){
+        let img = new Image()
+        img.src = this.atrib
+        des.drawImage(img,this.x,this.y,this.w,this.h)
     }
 }
 
 class Carro2 extends Object{
     move(){
-        this.y += 1
+        this.y += 3
         if(this.y >= 610){
             this.y = -100
             this.x = (Math.random() * (545 - 2 + 1) + 2)
         }
     }
+    des_obj(){
+        let img = new Image()
+        img.src = this.atrib
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+    }
 }
 
+class Trilho_esq extends Object{
+    des_obj(){
+        des.beginPath()
+        des.lineWidth = '5'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.rect(this.x,this.y,this.w,this.h);
+        des.closePath()
+        des.stroke()
+        des.fill()
+    }
+}
 
+class Trilho_dir extends Object{
+    des_obj(){
+        des.beginPath()
+        des.lineWidth = '5'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.rect(this.x,this.y,this.w,this.h);
+        des.closePath()
+        des.stroke()
+        des.fill()
+    }
+}
 
+class Pedais extends Object{
+    des_obj(){
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-550)
+        des.lineTo(this.x+70,this.y-550)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-500)
+        des.lineTo(this.x+70,this.y-500)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-450)
+        des.lineTo(this.x+70,this.y-450)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-400)
+        des.lineTo(this.x+70,this.y-400)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-350)
+        des.lineTo(this.x+70,this.y-350)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-300)
+        des.lineTo(this.x+70,this.y-300)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-250)
+        des.lineTo(this.x+70,this.y-250)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-200)
+        des.lineTo(this.x+70,this.y-200)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-150)
+        des.lineTo(this.x+70,this.y-150)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-100)
+        des.lineTo(this.x+70,this.y-100)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y-50)
+        des.lineTo(this.x+70,this.y-50)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y)
+        des.lineTo(this.x+70,this.y)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+50)
+        des.lineTo(this.x+70,this.y+50)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+100)
+        des.lineTo(this.x+70,this.y+100)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+150)
+        des.lineTo(this.x+70,this.y+150)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+200)
+        des.lineTo(this.x+70,this.y+200)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+250)
+        des.lineTo(this.x+70,this.y+250)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+300)
+        des.lineTo(this.x+70,this.y+300)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+350)
+        des.lineTo(this.x+70,this.y+350)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+400)
+        des.lineTo(this.x+70,this.y+400)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+450)
+        des.lineTo(this.x+70,this.y+450)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+500)
+        des.lineTo(this.x+70,this.y+500)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+550)
+        des.lineTo(this.x+70,this.y+550)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+600)
+        des.lineTo(this.x+70,this.y+600)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        des.beginPath()
+        des.lineWidth = '10'
+        des.fillStyle = this.atrib
+        des.strokeStyle = 'black'
+        des.moveTo(this.x,this.y+650)
+        des.lineTo(this.x+70,this.y+650)
+        des.closePath()
+        des.stroke()
+        des.fill()
+    }
+
+    move(){
+        this.y += 5
+        if(this.y >= 610){
+            this.y = -100
+        }
+    }
+}
+
+class Arvore extends Object{
+    move(){
+        this.y += 5
+        if(this.y >= 610){
+            this.y = -100
+        }
+    }
+
+    des_obj(){
+        let img = new Image()
+            img.src = this.atrib
+            des.drawImage(img,this.x,this.y,this.w,this.h)
+    }
+}
+    
 // classe estrada
 
 // classe texto
+
+class Text{
+    des_text(text,x,y,cor,font){
+        des.fillStyle = cor
+        des.lineWidth = '5'
+        des.font = font
+        des.fillText(text,x,y)
+    }
+}
--- car.js
+++ car.js
@@ -1,22 +1,31 @@
 // leitura canvas
 let des = document.getElementById('des').getContext('2d')
 // definição das instâncias
-const train = new Train(260, 500, 50, 30,'orange')
-const carro1 = new Carro1(120, 100, 50, 30,'darkgreen')
-const carro2 = new Carro2(280, 200, 50, 30,'pink')
+const train = new Train(260, 500, 150, 120,'./assets/img/tomas.png')
+const carro1 = new Carro1(120, 100, 80, 120,'./assets/img/scooby_doo.png')
+const carro2 = new Carro2(280, 200, 80, 120,'./assets/img/vesuvio.png')
+const trilho_esq = new Trilho_esq(250, 0, 8, 600,'gray')
+const trilho_dir = new Trilho_dir(310, 0, 8, 600,'gray')
+const pedais = new Pedais(250, 20, 10, 60,'gray')
+const arvore = new Arvore(220, 50, 1, 1,'./assets/img/tree.png')
 
 // definição dos textos
+const t1 = new Text()
 
 // definição das instâncias do som
+const som1 = new Audio('./assets/audio/nature.mp3')
+som1.volume = 1.0
+som1.loop = true
 
 // controles de movimentação
 
 document.addEventListener('keydown', (e)=>{
     if(e.key === 'a'){
-        train.x -= 5
-    }else if(e.key === 'd'){
-        train.x += 5
+        train.x -= 13
+    }if(e.key === 'd'){
+        train.x += 13
     }
+    som1.play()
 })
 
 
@@ -28,6 +37,11 @@ document.addEventListener('keydown', (e)=>{
 
 // desenha
 function desenha(){
+    t1.des_text('Pontos : ', 10, 60, '#000', 'bold 50px system-ui')
+    trilho_esq.des_obj()
+    trilho_dir.des_obj()
+    arvore.des_obj()
+    pedais.des_obj()
     carro1.des_obj()
     carro2.des_obj()
     train.des_obj()
@@ -35,6 +49,8 @@ function desenha(){
 
 // atualiza
 function atualiza(){
+    pedais.move()
+    arvore.move()
     train.move()
     carro1.move()
     carro2.move()
@@ -45,6 +61,7 @@ function main(){
     des.clearRect(0,0,600,600)
     atualiza()
     desenha()
+    requestAnimationFrame(main)
 }
 
-setInterval(main,10)
+main()
